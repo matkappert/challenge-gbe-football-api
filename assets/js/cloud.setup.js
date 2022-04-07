ef03feaf4de43c04dbec07cbd5a3905ead7b649f
Mat Kappert <matkappert@gmail.com>
2022-04-07 16:29:14 +1000

    Routes updated with CloudSDK

---
+++ assets/js/cloud.setup.js
@@ -0,0 +1,19 @@
+/**
+ * cloud.setup.js
+ *
+ * Configuration for this Sails app's generated browser SDK ("Cloud").
+ *
+ * Above all, the purpose of this file is to provide endpoint definitions,
+ * each of which corresponds with one particular route+action on the server.
+ *
+ * > This file was automatically generated.
+ * > (To regenerate, run `sails run rebuild-cloud-sdk`)
+ */
+
+Cloud.setup({
+
+  /* eslint-disable */
+  methods: {"getTeams":{"verb":"GET","url":"/api/v1/teams","args":[]}}
+  /* eslint-enable */
+
+});
